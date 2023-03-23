ab91823f2e4bf54431d9fb7803276c41b5e65d62
@Bahatimyk <manenomichael99@gmail.com>
2023-03-23 11:20:13 +0300

    create user api call code

--- src/Admin/AdminUsers/AdminNewUsers.js
+++ src/Admin/AdminUsers/AdminNewUsers.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AdminSidebar from '../AdminSidebar/AdminSidebar';
 import './AdminUsers.css';
 import * as adminnew from 'react-bootstrap';
 import { Link } from 'react-router-dom'; 
+import axios from 'axios';
 
 
 function AdminNewUsers() {
+
+const [newuser, setnewuser] = useState({
+  name: '',
+  email: '',
+  phone_number: '',
+  id_number: '',
+  location: '',
+  designition: '',
+});
+
+const handleinput = (e) =>{
+  e.persist();
+
+  setnewuser({...newuser, [e.target.name]: e.target.value})
+};
+
+const createuser = (e) =>{
+  e.preventDefault();
+
+  const data={
+    name: newuser.name,
+    email: newuser.email,
+    phone_number: newuser.phone_number,
+    id_number: newuser.id_number,
+    location: newuser.location,
+    designition: newuser.designition,
+  };
+
+  try {
+    axios.post(`api/createuser`, data).then((res)=>{
+      console.log(res);
+    })
+    .catch((res)=>{
+      console.log(res);
+    })
+  } catch (error) {
+    
+  }
+}
+
   return (
     <div>
         <AdminSidebar />
@@ -25,12 +66,12 @@ function AdminNewUsers() {
                         <div className='addin d-flex mb-3'>
                           <div>
                            <label>Name</label>
-                           <input name='name' className='form-control shadow-none ' type="text" />
+                           <input name='name' onChange={handleinput} value={newuser.name} className='form-control shadow-none ' type="text" />
                           </div>
                       
                            <div>
                             <label>Email adress</label>
-                            <input name='email' className='form-control shadow-none ' type="text" />
+                            <input name='email' onChange={handleinput} value={newuser.email} className='form-control shadow-none ' type="text" />
                            </div>
                         </div>
                       
@@ -39,11 +80,11 @@ function AdminNewUsers() {
                       <div className='addin d-flex mb-3'>
                           <div>
                              <label>Phone number</label>
-                             <input name='phone_number' className='form-control shadow-none ' type="text" />
+                             <input name='phone_number' onChange={handleinput} value={newuser.phone_number} className='form-control shadow-none ' type="text" />
                           </div>
                           <div>
                              <label>Id Number</label>
-                             <input name='id_number' className='form-control shadow-none ' type="text" />
+                             <input name='id_number' onChange={handleinput} value={newuser.id_number} className='form-control shadow-none ' type="text" />
                           </div>
                           
                       </div>
@@ -51,18 +92,18 @@ function AdminNewUsers() {
                       <div className='addin d-flex mb-5'>
                           <div>
                              <label>Location</label>
-                             <input  name='location' className='form-control shadow-none ' type="text" />
+                             <input  name='location' onChange={handleinput} value={newuser.location} className='form-control shadow-none ' type="text" />
                           </div>
                           <div>
                              <label>Designation</label>
-                             <input  name='location' className='form-control shadow-none ' type="text" />
+                             <input  name='designition' onChange={handleinput} value={newuser.designition} className='form-control shadow-none ' type="text" />
                           </div>
                       </div>
                       
                     
                   
                       <div className='details2 d-flex mb-3'>
-                            <button className='btn btn-success'>Create User</button>
+                            <button onClick={createuser} className='btn btn-success'>Create User</button>
                       </div>
 
                     </adminnew.Card.Body>
