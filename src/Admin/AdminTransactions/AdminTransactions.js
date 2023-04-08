04ebc5defeb321b8e9f52e493988116c394bae16
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:27:53 +0300

    admintranc bars

--- src/Admin/AdminTransactions/AdminTransactions.js
+++ src/Admin/AdminTransactions/AdminTransactions.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminSidebar from '../AdminSidebar/AdminSidebar';
 import * as admintrans from 'react-bootstrap';
 import './AdminTransactions.css';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function AdminTransactions() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <AdminSidebar />
+        <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <AdminSidebar />
+          </div>
+        )}
+      </div>
+
 
         <admintrans.Container>
             
