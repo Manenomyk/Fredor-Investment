62c82bd2c41e7234c763dde5b002affb61971faa
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:45:30 +0300

    clerktrans bars

--- src/Clerk/ClerkTransactions/ClerkTransactions.js
+++ src/Clerk/ClerkTransactions/ClerkTransactions.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ClerkTransactions.css';
 import * as clerktrans from 'react-bootstrap';
 import ClerkSidebar from '../ClerkSidebar/ClerkSidebar';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function ClerkTransactions() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <ClerkSidebar />
+        <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <ClerkSidebar />
+          </div>
+        )}
+      </div>
 
 
         <clerktrans.Container>
