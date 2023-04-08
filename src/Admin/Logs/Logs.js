090c529068a9265a6d3aba72b00759921e66253b
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:34:39 +0300

    logs bars

--- src/Admin/Logs/Logs.js
+++ src/Admin/Logs/Logs.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Logs.css';
 import AdminSidebar from '../AdminSidebar/AdminSidebar';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function Logs() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <AdminSidebar />
+        <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <AdminSidebar />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
