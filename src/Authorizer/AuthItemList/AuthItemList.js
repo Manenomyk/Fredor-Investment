24215b8555f05b3f607e697e4bae1fcdcae8c2cc
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 15:04:08 +0300

    authitemlist bars

--- src/Authorizer/AuthItemList/AuthItemList.js
+++ src/Authorizer/AuthItemList/AuthItemList.js
@@ -4,8 +4,11 @@ import "./AuthItemList.css";
 import * as authlist from "react-bootstrap";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function AuthItemList() {
+  const [isOpen, setIsOpen] = useState(false);
   const [items, setitems] = useState([]);
   useEffect(() => {
     axios.get(`api/adminViewitems`).then((res) => {
@@ -18,7 +21,36 @@ function AuthItemList() {
 
   return (
     <div>
-      <AuthSidebar />
+      <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+           <AuthSidebar />
+          </div>
+        )}
+      </div>
+
 
       <authlist.Container>
         <authlist.Row>
