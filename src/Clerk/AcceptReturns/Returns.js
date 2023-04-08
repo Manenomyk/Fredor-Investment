19fc751c40d8761127f256095013754f53eaf69b
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:39:20 +0300

    return bars

--- src/Clerk/AcceptReturns/Returns.js
+++ src/Clerk/AcceptReturns/Returns.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AcceptReturns.css';
 import ClerkSidebar from '../ClerkSidebar/ClerkSidebar';
 import * as returns from 'react-bootstrap';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function Returns() {
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <ClerkSidebar />
+        <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <ClerkSidebar />
+          </div>
+        )}
+      </div>
+
 
         <returns.Container>
         <returns.Row>
