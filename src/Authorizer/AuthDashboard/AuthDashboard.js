6679d7d5d0066d5932aecd1d56f88ac06e22994c
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:03:02 +0300

    authdash bars

--- src/Authorizer/AuthDashboard/AuthDashboard.js
+++ src/Authorizer/AuthDashboard/AuthDashboard.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import AuthSidebar from '../AuthSidebar/AuthSidebar';
 import * as authdash from 'react-bootstrap';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function AuthDashboard() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <AuthSidebar />
+      <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+           <AuthSidebar />
+          </div>
+        )}
+      </div>
+
+        
         <authdash.Container>
         <authdash.Row className="mt-4">
           <authdash.Col className="mx-auto mb-3" lg={3} >
