034c9ae5d8ce95f717fb453480f29ce973844591
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 13:53:07 +0300

    clerk sidebar resposinveness

--- src/Clerk/ClerkDashboard/ClerkDashboard.js
+++ src/Clerk/ClerkDashboard/ClerkDashboard.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClerkSidebar from '../ClerkSidebar/ClerkSidebar';
 import * as clerkdash from 'react-bootstrap';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function ClerkDashboard() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
-      <ClerkSidebar />
+      <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <ClerkSidebar />
+          </div>
+        )}
+      </div>
+
+      
       <clerkdash.Container>
         <clerkdash.Row className="mt-4">
           <clerkdash.Col className="mx-auto mb-3" lg={3} >
