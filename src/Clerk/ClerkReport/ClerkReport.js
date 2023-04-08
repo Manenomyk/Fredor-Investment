149a4ef9697d53a633c9d70bb85101d33fd99cc2
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:43:52 +0300

    clerkrepo bars

--- src/Clerk/ClerkReport/ClerkReport.js
+++ src/Clerk/ClerkReport/ClerkReport.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClerkSidebar from '../ClerkSidebar/ClerkSidebar';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function ClerkReport() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <ClerkSidebar />
+       <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <ClerkSidebar />
+          </div>
+        )}
+      </div>
+
     </div>
   )
 }
