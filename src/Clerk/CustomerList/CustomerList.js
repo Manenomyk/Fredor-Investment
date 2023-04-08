95c1402fb4b47fe71a60da95459ea43af2046604
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 14:49:40 +0300

    clerk customer bars

--- src/Clerk/CustomerList/CustomerList.js
+++ src/Clerk/CustomerList/CustomerList.js
@@ -3,8 +3,11 @@ import ClerkSidebar from '../ClerkSidebar/ClerkSidebar';
 import './CustomerList.css';
 import * as custolist from 'react-bootstrap';
 import axios from 'axios';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function CustomerList() {
+  const [isOpen, setIsOpen] = useState(false);
   const [customerlist, setcustomerlist] = useState([]);
 
   useEffect(() => {
@@ -18,7 +21,35 @@ function CustomerList() {
   }, []);
   return (
     <div>
-      <ClerkSidebar />
+      <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <ClerkSidebar />
+          </div>
+        )}
+      </div>
 
       <custolist.Container className=''>
           <custolist.Row>
