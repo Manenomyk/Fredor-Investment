ef90366cd34ef37e0150640a24ef2783b5446c2f
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 15:09:00 +0300

    authreport bars

--- src/Authorizer/AuthReport/AuthReport.js
+++ src/Authorizer/AuthReport/AuthReport.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthSidebar from '../AuthSidebar/AuthSidebar';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function AuthReport() {
+  
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-        <AuthSidebar />
+        <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+           <AuthSidebar />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
