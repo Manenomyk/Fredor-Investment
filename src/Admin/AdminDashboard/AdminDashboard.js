7a71f9f027e13210cf4ec08582be616eb5957a50
@Bahatimyk <manenomichael99@gmail.com>
2023-04-08 13:59:59 +0300

    admin bars

--- src/Admin/AdminDashboard/AdminDashboard.js
+++ src/Admin/AdminDashboard/AdminDashboard.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminSidebar from "../AdminSidebar/AdminSidebar";
 import * as dash from "react-bootstrap";
 import './AdminDashboard.css';
+import { FaBars } from "react-icons/fa";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 function AdminDashboard() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="admindash">
-      <AdminSidebar />
+      <div>
+        {!isOpen ? (
+          <div
+            style={{
+              marginTop: "1rem",
+              fontSize: "25px",
+              cursor: "pointer",
+              marginLeft: "1rem",
+            }}
+          >
+            <FaBars onClick={() => setIsOpen(!isOpen)} />
+          </div>
+        ) : (
+          <div>
+            <IoIosArrowDropleft
+              onClick={() => setIsOpen(!isOpen)}
+              style={{
+                fontSize: "2rem",
+                color: "#007CBA",
+                position: "fixed",
+                top: "2%",
+                marginLeft: "187px",
+                cursor: "pointer",
+              }}
+            />
+            <AdminSidebar />
+          </div>
+        )}
+      </div>
+
+      
 
       <dash.Container>
         <dash.Row className="mt-4">
