e16aac2c631380f90f045d9cbf9a9421cf5c3603
@Bahatimyk <manenomichael99@gmail.com>
2023-04-17 18:26:30 +0300

    update profile code

--- src/Admin/AdminProfile/AdminProfile.js
+++ src/Admin/AdminProfile/AdminProfile.js
@@ -1,72 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import AdminSidebar from '../AdminSidebar/AdminSidebar';
-import * as adminprof from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import AdminSidebar from "../AdminSidebar/AdminSidebar";
+import * as adminprof from "react-bootstrap";
 import { IoMdContact } from "react-icons/io";
-import './AdminProfile.css'
-import axios from 'axios';
+import "./AdminProfile.css";
+import axios from "axios";
 import { FaBars } from "react-icons/fa";
 import { IoIosArrowDropleft } from "react-icons/io";
 
-
-const id = localStorage.getItem('userID');
+const id = localStorage.getItem("userID");
 function AdminProfile() {
   const [isOpen, setIsOpen] = useState(false);
   const [errors, seterrors] = useState([]);
- 
-  
+
   const [adminpro, setadminpro] = useState({
-    name : "",
+    name: "",
     email: "",
     phone_number: "",
     id_number: "",
     location: "",
-
   });
   useEffect(() => {
     // const userid=props.match.params.id;
-   axios.get(`/api/view_profile/${id}`).then(res=>{
-    console.log(res);
-    if (res.data.status === 200) {
-      setadminpro(res.data.profile)
-    }
-    else if (res.data.status === 404) {
-      alert('not a user');
-    }
-   });
-  }, []);
-
-const handleinput = (e)=>{
-  e.persist();
-  setadminpro({...adminpro, [e.target.name]: e.target.value});
-};
-
-const adminupdate = (e) =>{
-  e.preventDefault();
-
-  const data ={
-    name: adminpro.name,
-    email: adminpro.email,
-    phone_number: adminpro.phone_number,
-    id_number: adminpro.id_number,
-    location: adminpro.location,
-  };
-
-    axios.put(`api/updateprofile/${id}`, data).then((res)=>{
+    axios.get(`/api/view_profile/${id}`).then((res) => {
       console.log(res);
       if (res.data.status === 200) {
-        
-      }else if(res.data.status === 422){
-        seterrors(res.data.validation_errors)
+        setadminpro(res.data.profile);
+      } else if (res.data.status === 404) {
+        alert("not a user");
       }
-    })
-    
- 
-}
+    });
+  }, []);
+
+  const handleinput = (e) => {
+    e.persist();
+    setadminpro({ ...adminpro, [e.target.name]: e.target.value });
+  };
+
+  const adminupdate = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name: adminpro.name,
+      email: adminpro.email,
+      phone_number: adminpro.phone_number,
+      id_number: adminpro.id_number,
+      location: adminpro.location,
+    };
+
+    axios.put(`api/updateprofile/${id}`, data).then((res) => {
+      console.log(res);
+      if (res.data.status === 200) {
+        alert("profile updated successfully");
+        seterrors([]);
+      } else if (res.data.status === 422) {
+        seterrors(res.data.validation_errors);
+      } else if (res.data.status === 404) {
+        alert("Ooops, User id not found");
+      } else {
+        alert("please contact admin");
+      }
+    });
+  };
 
   return (
-    <div >
-
-<div>
+    <div>
+      <div>
         {!isOpen ? (
           <div
             style={{
@@ -96,62 +94,98 @@ const adminupdate = (e) =>{
         )}
       </div>
 
-        
-                                    
-            <adminprof.Container>
-              <adminprof.Row>
-                <adminprof.Col className='mx-auto' lg={7}>
-                  <adminprof.Card className='profi'>
-                    <adminprof.Card.Title>
-                      
-                        <div className='edit'>Edit profile</div>
-                        <div className='profiicon'>
-                          
-                        <div ><IoMdContact/></div>
-                        
-                      </div>
-                    </adminprof.Card.Title>
-                    <adminprof.Card.Body className='mx-auto'>
-                      <div>
-                        <div>Name</div>
-                        <input name='name' onChange={handleinput} value={adminpro.name} className='form-control shadow-none profinput' type="text" />
-                      </div>
-                      
-                      <div>
-                        <div>Email adress</div>
-                        <input name='email' onChange={handleinput} value={adminpro.email} className='form-control shadow-none profinput' type="text" />
-                      </div>
-                      <div>
-                        <div>Phone number</div>
-                        <input name='phone_number' onChange={handleinput} value={adminpro.phone_number} className='form-control shadow-none profinput' type="text" />
-                      </div>
-                      <div>
-                        <div>Id Number</div>
-                        <input name='id_number' onChange={handleinput} value={adminpro.id_number} className='form-control shadow-none profinput' type="text" />
-                      </div>
-                      <div>
-                        <div>Location</div>
-                        <input  name='location' onChange={handleinput} value={adminpro.location} className='form-control shadow-none profinput' type="text" />
-                      </div>
-                    
-                  
-                      
+      <adminprof.Container>
+        <adminprof.Row>
+          <adminprof.Col className="mx-auto" lg={7}>
+            <adminprof.Card className="profi">
+              <adminprof.Card.Title>
+                <div className="edit">Edit profile</div>
+                <div className="profiicon">
+                  <div>
+                    <IoMdContact />
+                  </div>
+                </div>
+              </adminprof.Card.Title>
+              <adminprof.Card.Body className="mx-auto">
+                <div>
+                  <div>Name</div>
+                  <input
+                    name="name"
+                    onChange={handleinput}
+                    value={adminpro.name}
+                    className="form-control shadow-none profinput"
+                    type="text"
+                  />
+                </div>
+                <small className="text-danger">{errors.name}</small>
 
-                      <div>
-                          <button onClick={adminupdate} className='btn btn-success' id='updatebtn'>Update</button>
-                                  
-                      </div>
-                      <a href="" id='uplink'>Update password?</a>
+                <div>
+                  <div>Email adress</div>
+                  <input
+                    name="email"
+                    onChange={handleinput}
+                    value={adminpro.email}
+                    className="form-control shadow-none profinput"
+                    type="text"
+                  />
+                </div>
 
-                    </adminprof.Card.Body>
-                  </adminprof.Card>
-                </adminprof.Col>
-              </adminprof.Row>
-            </adminprof.Container>
+                <small className="text-danger">{errors.email}</small>
+                <div>
+                  <div>Phone number</div>
+                  <input
+                    name="phone_number"
+                    onChange={handleinput}
+                    value={adminpro.phone_number}
+                    className="form-control shadow-none profinput"
+                    type="text"
+                  />
+                </div>
 
+                <small className="text-danger">{errors.phone_number}</small>
+                <div>
+                  <div>Id Number</div>
+                  <input
+                    name="id_number"
+                    onChange={handleinput}
+                    value={adminpro.id_number}
+                    className="form-control shadow-none profinput"
+                    type="text"
+                  />
+                </div>
 
+                <small className="text-danger">{errors.id_number}</small>
+                <div>
+                  <div>Location</div>
+                  <input
+                    name="location"
+                    onChange={handleinput}
+                    value={adminpro.location}
+                    className="form-control shadow-none profinput"
+                    type="text"
+                  />
+                </div>
+                <small className="text-danger">{errors.location}</small>
+
+                <div>
+                  <button
+                    onClick={adminupdate}
+                    className="btn btn-success"
+                    id="updatebtn"
+                  >
+                    Update
+                  </button>
+                </div>
+                <a href="" id="uplink">
+                  Update password?
+                </a>
+              </adminprof.Card.Body>
+            </adminprof.Card>
+          </adminprof.Col>
+        </adminprof.Row>
+      </adminprof.Container>
     </div>
-  )
+  );
 }
 
-export default AdminProfile
+export default AdminProfile;
